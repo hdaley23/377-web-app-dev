24c14b0fe93a87e6315ba429a67c589ed0ab09da
Hayden Daley <hdaley23@hanoverstudents.org>
2022-09-28 12:42:21 -0400

    final version

--- 2-equation-calculator/scripts.js
+++ 2-equation-calculator/scripts.js
@@ -1,3 +1,4 @@
+// FOR A CUBE
 function calculate(equation){
     var value1  = parseInt(document.getElementById("value1").value);
     var value2 = parseInt(document.getElementById("value2").value);  
@@ -13,7 +14,7 @@ function calculate(equation){
         result = volume(value1, value2, value3);
     }
     
-    document.getElementById("result").innerHTML = result;
+    document.getElementById("result1").innerHTML = result;
 } 
 
 function perimeter(value1, value2) {
@@ -28,4 +29,34 @@ function volume(value1, value2, value3) {
     return value1 * value2 * value3;
 }
 
+// FOR A PYRAMID 
+function calculateTri(equation){
+    var value4  = parseInt(document.getElementById("value4").value);
+    var value5 = parseInt(document.getElementById("value5").value);  
+    var value6 = parseInt(document.getElementById("value6").value); 
+    
+    var result = perimeterTri(value4, value5);
+
+    if(equation == 'perimeter'){
+        result = perimeterTri(value4, value5);
+    }else if(equation == 'area'){
+        result = areaTri(value4, value5);
+    }else if(equation == 'volume'){
+        result = volumeTri(value4, value5, value6);
+    }
+    
+    document.getElementById("result2").innerHTML = result;
+} 
+
+function perimeterTri(value4, value5) {
+    return value4 + value5 + value4 + value5;
+}
+
+function areaTri(value4, value5) {
+    return value4 * value5;
+}
+
+function volumeTri(value4, value5,  value6) {
+    return 1/3 * value4 * value5 * value6;
+}
 
