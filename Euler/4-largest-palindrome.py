2c710cd41c203e4a41436a1738c707744ab4fab8
Hayden Daley <hdaley23@hanoverstudents.org>
2023-01-23 12:23:11 -0500

    final

--- Euler/4-largest-palindrome.py
+++ Euler/4-largest-palindrome.py
@@ -1,8 +1,8 @@
 largest = 0
 for i in range(999, 100, -1):
     for j in range(i, 100, -1):
-        x = i * j
-        if x > largest:
+        num = i * j
+        if num > largest:
             palindrome = str(i * j)
             if palindrome == palindrome[::-1]:
                 largest = i * j
